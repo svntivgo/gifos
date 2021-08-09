2108ec89900e45e2ff172bc6cb641ffcf35b94f3
Santiago Barrera <santiago.barrerae@gmail.com>
2021-08-09 16:19:32 -0500

    fav button on saved as active css class

--- js/modules/constructor/components/buttons/favorite/favorite.js
+++ js/modules/constructor/components/buttons/favorite/favorite.js
@@ -1,7 +1,20 @@
-export let favorite = (section, index, favorites) => {
-    let favoriteBtn = document.getElementById(`${section}__gifo-button-favorite-${index}`)
+export let favorite = (section, id, favorites, arrayPush) => {
+
+    let favorite = favorites.find((fav) => fav.id === arrayPush[id].id)
+    let favoriteBtn = document.getElementById(`${section}__gifo-button-favorite-${id}`)
+
+    if (favorite.id === arrayPush[id].id) {
+        favoriteBtn.classList.add(`${section}__gifo-button-favorite-saved`)
+    }
+
     favoriteBtn.addEventListener('click', () => {
-        favorites.push(array)
-        localStorage.setItem('favorites', JSON.stringify(favorites))
+
+        if (favorite.id !== arrayPush[id].id) {
+
+            favorites.push(arrayPush[id])
+            localStorage.setItem('favorites', JSON.stringify(favorites))
+
+        }
+
     })
 }
--- js/modules/constructor/components/buttons/maximize/maximize.js
+++ js/modules/constructor/components/buttons/maximize/maximize.js
@@ -1,5 +1,7 @@
 export let maximize = (section, index, imgUrl, gifId, owner, title, id, arrayPush, favorites) => {
     let overlay = document.getElementsByClassName(`${section}__overlay`)
+    let favorite = favorites.find((fav) => fav.id === arrayPush[id].id)
+    let favoriteBtn = document.getElementById(`max__gifo-button-favorite`)
     overlay[id].addEventListener('click', () => {
         let maxContainer = document.getElementById('max__gifo-container')
         maxContainer.classList.toggle('max__gifo-container--maximized')
@@ -23,14 +25,21 @@ export let maximize = (section, index, imgUrl, gifId, owner, title, id, arrayPus
             </div>
         </div>
         `)
+
+        if (favorite.id === arrayPush[id].id) {
+            favoriteBtn.classList.add('max__gifo-button-favorite-saved')
+        }
+
         document.getElementById(`max__close`).addEventListener('click', () => {
             maxContainer.innerHTML = ('<div class="max__close" id="max__close"></div>')
             maxContainer.classList.toggle('max__gifo-container--maximized')
+            id = 0
+            return id
         })
 
         document.getElementById(`max__gifo-button-favorite`).addEventListener('click', () => {
-            favorites.push(arrayPush[id])
-            localStorage.setItem('favorites', JSON.stringify(favorites))
+            // favorites.push(arrayPush[id])
+            // localStorage.setItem('favorites', JSON.stringify(favorites))
         })
         document.getElementById(`max__gifo-button-download`).addEventListener('click', () => {
             window.open(`https://i.giphy.com/media/${arrayPush[id].id}/source.gif`, "_blank");
@@ -38,12 +47,24 @@ export let maximize = (section, index, imgUrl, gifId, owner, title, id, arrayPus
 
         document.getElementById(`max__next`).addEventListener('click', () => {
             if (id < arrayPush.length-1) {
+
                 id++
+
+                favorite = favorites.find((fav) => fav.id === arrayPush[id].id)
+                console.log(favorite);
+
                 let image = document.getElementById(`max__img`)
                 let texts = document.getElementById('max__gifo-text-container')
                 image.src = (`${arrayPush[id].images.preview_webp.url}`)
                 texts.firstElementChild.innerHTML = (`${arrayPush[id].username}`)
                 texts.lastElementChild.innerHTML = (`${arrayPush[id].title}`)
+
+                if (favorite.id === arrayPush[id].id) {
+                    favoriteBtn.classList.add('max__gifo-button-favorite-saved')
+                } else {
+                    favoriteBtn.classList.remove('max__gifo-button-favorite-saved')
+                }
+
                 return id
             }
         })
